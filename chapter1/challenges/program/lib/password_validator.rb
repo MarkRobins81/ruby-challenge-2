e220bd2e76393bbe79541662e81d0ef8acfcda3c
Mark Robins <markrobins81@gmail.com>
2022-12-05 11:26:50 +0000

    ready to be reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,9 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    if password.length >= 8
+        return password.include?("!") || password.include?("@") || password.include?("$") || password.include?("%") || password.include?("&")
+    else
+        return false
+    end
 end
